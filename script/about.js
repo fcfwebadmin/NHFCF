15937d4261378b2a9acf52f7a447f22c78e2ff00
RenceZ3 <rence.dappy@gmail.com>
2025-01-08 17:00:54 -0500

    Added About Page

---
+++ script/about.js
@@ -0,0 +1,33 @@
+let circle = document.querySelector(".circle-rotate");
+let textAbout = document.querySelector(".text-about");
+
+
+// window.addEventListener("scroll", () => {
+//     // const getHeight = circle.getBoundingClientRect().top;
+
+//     if(window.scrollY == 0) {
+//         circle.classList.remove("rotate-45");
+//     }else {
+//         circle.classList.add("rotate-45");
+//     }
+
+
+// });
+
+
+window.addEventListener("scroll", () => {
+    let getHeight = textAbout.getBoundingClientRect().top - 20;
+    console.log("test",getHeight);
+    
+    console.log(window.scrollY);
+    
+    if(getHeight <= window.scrollY) {
+        textAbout.classList.add("shrink-text");
+    }
+
+        
+    if(window.scrollY <= 0) {
+        textAbout.classList.remove("shrink-text");
+    }
+
+});
--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ let trees = document.querySelector('#trees');
 let bg = document.querySelector('#bg');
 let text = document.querySelector('#text');
 let subTitle = document.querySelector('.sub-title');
+
 window.addEventListener('scroll', () => {
     let scroll = window.scrollY;
 
@@ -13,3 +14,42 @@ window.addEventListener('scroll', () => {
     subTitle.style.marginTop = `${scroll * -0.2}px`;
 
 });
+
+
+let aboutSection = document.querySelector(".about");
+
+window.addEventListener("scroll", () => {
+
+    let revealText = document.querySelectorAll(".rev-text");
+
+    revealText.forEach((ele) => {
+        ele.classList.add("hide");
+        const heightEle = ele.getBoundingClientRect().top + 300;
+
+        if(window.innerHeight >= heightEle) {
+            ele.classList.add("reveal");
+        }
+    });
+    
+});
+
+
+let aboutDrop = document.querySelector(".drop-about");
+let isClicked = false;
+
+aboutDrop.addEventListener("click", function(event) {
+
+    isClicked = !isClicked;    
+
+    if(isClicked) {
+        aboutDrop.children[0].classList.remove("hidden");
+    }else {
+        aboutDrop.children[0].classList.add("hidden");
+    }
+});
+
+aboutDrop.addEventListener("mouseleave", () => {
+    isClicked = false;
+    aboutDrop.children[0].classList.add("hidden");
+});
+
